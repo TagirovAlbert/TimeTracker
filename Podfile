6609399c835fba325a15609950d4c3cfd44a8537
Albert Tagirov <bert_tagirov@mail.ru>
2018-07-12 22:54:14 +0300

    prepare for release (1.0)

--- Podfile
+++ Podfile
@@ -7,3 +7,10 @@ target 'Stopwatch' do
   pod "RealmSwift"
   pod 'FSCalendar'
 end
+
+post_install do |installer|
+    installer.pods_project.build_configurations.each do |config|
+        config.build_settings.delete('CODE_SIGNING_ALLOWED')
+        config.build_settings.delete('CODE_SIGNING_REQUIRED')
+    end
+end
--- Stopwatch/AppDelegate.swift
+++ Stopwatch/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        sleep(1)
         UINavigationBar.appearance().barTintColor = .black
         UINavigationBar.appearance().tintColor = .white
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
